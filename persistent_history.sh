946e3b767cd314c0834b265ae34ac17adbdc54ae
Danny Kirchmeier <danny@kirchmeier.us>
2024-06-23 21:11:37 -0500

    supercharge persistent history

--- persistent_history.sh
+++ persistent_history.sh
@@ -1,9 +1,17 @@
 preexec() {
-  print -rN -- "$(date +%Y-%m-%dT%H:%M:%S) $3" >> ~/.persistent_history
+  print -N -- "$PWD\t$(date +%F%t%T)\t$3" >> ~/.persistent_history
 }
 
 __persistent-history-action() {
-  local output="$(fzf --query="$BUFFER" --read0 --reverse --tac --no-sort -n 2.. < ~/.persistent_history | sed -E 's/[^ ]* //')"
+  local output="$(\
+    fzf --query="$1" --read0 --tac --no-sort \
+    --with-nth 4.. --delimiter $'\t' \
+    --preview='print -rD -- {2} {3} " " {1}' \
+    --preview-label="ctrl+space to run in dir" \
+    --preview-window='bottom,1,border-top' \
+    --bind='enter:become(echo {4..})' \
+    --bind='ctrl-space:become(echo cd {1} "&&" {4..})' \
+    < ~/.persistent_history)"
   if [[ -n "$output" ]] ; then
     LBUFFER="${output}"
     RBUFFER=""
